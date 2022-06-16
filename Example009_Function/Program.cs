cfac307ca39faeac76943f7902817452c07246ef
konstantin <ognev.k.a@mail.ru>
2022-06-16 13:56:52 +0300

    add Example009_Function

---
+++ Example009_Function/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] ArrMaxMin(int[] arr)
+{
+	for (int i = 0; i < arr.Length - 1; i++)
+	{
+		int maxPosition = i;
+		for (int j = i + 1; j < arr.Length; j++)
+		{
+			if (arr[j] > arr[maxPosition]) maxPosition = j;
+		}
+		int temp = arr[i];
+		arr[i] = arr[maxPosition];
+		arr[maxPosition] = temp;
+	}
+	return arr;
+}
+
+int[] arr = { 3, 3, 7, 1, 45, 32, 89, 15 };
+
+Console.Write(string.Join(", ", ArrMaxMin(arr)));
